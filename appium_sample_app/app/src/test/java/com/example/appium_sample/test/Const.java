b8e4bb039e8e341d448b7a896c44aee4c93fd0eb
kishandonga <kishandonga.92@gmail.com>
2019-03-11 13:17:49 +0530

    Jar changed and removed licence files of the Jar, added Data Provider and Const Class

---
+++ appium_sample_app/app/src/test/java/com/example/appium_sample/test/Const.java
@@ -0,0 +1,5 @@
+package com.example.appium_sample.test;
+
+public class Const {
+    public static final String SELECTED_ITEM = "_SelectedItem";
+}
--- appium_sample_app/app/src/test/java/com/example/appium_sample/test/ExecuteFruitsTest.java
+++ appium_sample_app/app/src/test/java/com/example/appium_sample/test/ExecuteFruitsTest.java
@@ -2,56 +2,44 @@ package com.example.appium_sample.test;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 /*
  *   Created By Kishan Donga 3/10/19
  */
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExecuteFruitsTest {
 
-    private static WebDriver driver;
+    private static AndroidDriver driver;
+    private static DataProvider provider;
 
     @BeforeClass
     public static void setUp() throws MalformedURLException {
+
+        provider = DataProvider.getInstance();
+
         // Created object of DesiredCapabilities class.
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        // Set android deviceName desired capability. Set your device name.
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-
-        // Set android VERSION desired capability. Set your mobile device's OS version.
-        capabilities.setCapability(CapabilityType.VERSION, "7.1.1");
-
-        // Set android platformName desired capability. It's Android in our case here.
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1.1");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-
-        // Set android appPackage desired capability. It is
-        // com.android.calculator2 for calculator application.
-        // Set your application's appPackage if you are using any other app.
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         capabilities.setCapability("appPackage", "com.example.appium_sample");
-
-        // Set android appActivity desired capability. It is
-        // com.android.calculator2.Calculator for calculator application.
-        // Set your application's appPackage if you are using any other app.
         capabilities.setCapability("appActivity", "com.example.appium_sample.ui.LoginAct");
-
-        // Created object of RemoteWebDriver will all set capabilities.
-        // Set appium server address and port number in URL string.
-        // It will launch calculator app in android device.
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
@@ -61,11 +49,11 @@ public class ExecuteFruitsTest {
     }
 
     @Test
-    public void testCashLogin() {
+    public void stage1_testCashLogin() {
 
-        WebElement email = driver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'edEmail')]"));
-        WebElement pwd = driver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'edPwd')]"));
-        WebElement btnLogin = driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'btnLogin')]"));
+        WebElement email = driver.findElementByXPath("//android.widget.EditText[contains(@resource-id,'edEmail')]");
+        WebElement pwd = driver.findElementByXPath("//android.widget.EditText[contains(@resource-id,'edPwd')]");
+        WebElement btnLogin = driver.findElementByXPath("//android.widget.Button[contains(@resource-id,'btnLogin')]");
 
         email.sendKeys("");
         btnLogin.click();
@@ -84,13 +72,25 @@ public class ExecuteFruitsTest {
 
         pwd.sendKeys("123");
         btnLogin.click();
-
     }
 
-    private void testCaseDashboard() {
-
-        driver.findElement(By.xpath("//android.support.v7.widget.RecyclerView[0]/android.widget.TextView")).click();
-
+    @Test
+    public void stage2_testCaseDashboard() {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement rvItem = driver.findElementByXPath("//android.support.v7.widget.RecyclerView[@index='0']/android.widget.TextView");
+        provider.put(Const.SELECTED_ITEM, rvItem.getText());
+
+        rvItem.click();
+    }
+
+    @Test
+    public void stage3_testCaseVerifyData() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement tvFruit = driver.findElementById("com.example.appium_sample:id/tvFruit");
+        String fruit = tvFruit.getText();
+        String selectedFruit = (String) provider.get(Const.SELECTED_ITEM);
+        if (fruit.equals(selectedFruit)) {
+            System.out.println("Both are Same Test Case Pass!");
+        }
     }
 }
